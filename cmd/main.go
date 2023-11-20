947b2dfbc6bf5aff3efdf65d7bccf490cbd406db
Endy Apina <39262413+endyApina@users.noreply.github.com>
2023-11-20 16:15:40 +0000

    update swagger docs (#30)

--- cmd/main.go
+++ cmd/main.go
@@ -17,7 +17,7 @@ import (
 // @contact.name	LEETA Technologies
 // @contact.email	admin@getlleta.com
 // @license.name	Apache 3.0-or-later
-// @host			localhost:3000
+// @host			https://leetabackend-e6d948d15ae2.herokuapp.com/api/
 // @BasePath		/api
 // @securityDefinitions.apikey BearerToken
 // @in header
--- docs/docs.go
+++ docs/docs.go
@@ -1711,7 +1711,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:3000",
+	Host:             "https://leetabackend-e6d948d15ae2.herokuapp.com/api/",
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "LEETA BACKEND API",
