6144917debced1d3c89d1d49d4ef5a13342df213
Endy Apina <39262413+endyApina@users.noreply.github.com>
2024-03-09 16:41:17 +0000

    get env variable from cli arguement

--- adapt/config.go
+++ adapt/config.go
@@ -46,16 +46,16 @@ type LeetaConfig struct {
 	Domain string `env:"DOMAIN"`
 }
 
-func LoadEnv() error {
-	err := godotenv.Load("./local.env")
+func LoadEnv(configFile string) error {
+	err := godotenv.Load(configFile)
 	if err != nil {
 		return fmt.Errorf("failed to load environment variables: %v", err)
 	}
 	return nil
 }
 
-func ReadConfig(logger zap.Logger) (*ServerConfig, error) {
-	err := LoadEnv()
+func ReadConfig(logger zap.Logger, configFile string) (*ServerConfig, error) {
+	err := LoadEnv(configFile)
 	if err != nil {
 		return nil, err
 	}
